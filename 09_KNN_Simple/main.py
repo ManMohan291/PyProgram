fd9b084ba5b6289acd92a5433ce0898b4c718c2c
ManMohan Singh <msingh.ebix.com>
2018-09-07 09:44:58 +0530

    KNN

---
+++ 09_KNN_Simple/main.py
@@ -0,0 +1,23 @@
+
+
+import KNN as K
+
+
+K.clearScreen()
+dataTraining= K.loadData("dataTraining.txt")
+
+X=dataTraining[:,0:2]
+
+
+initial_centroids=K.listToArray([[3, 3],[6, 2],[8,5]])
+
+idx=K.KMean_Run(X,initial_centroids,20)
+
+
+
+
+K.plotKNN(X,idx)
+
+
+
+
